2686d3af3252f3fc759aebbdceaa4b9e858d7bdd
Scott Cox <jscox5@gmail.com>
2022-09-24 11:56:31 -0600

    completed User model

--- Cheers!/Models/User.swift
+++ Cheers!/Models/User.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct User {
+class User: Codable {
     
     enum Keys {
         static let username = "username"
@@ -33,5 +33,27 @@ struct User {
          Keys.email : self.email,
          Keys.uuid : self.uuid,
          Keys.customCocktails : self.customCocktails.map {$0.cocktailData}]
+    }
+    
+    init(email: String, uuid: String, customcocktails: [CustomCocktail], collectionType: String = "users") {
+        self.email = email
+        self.uuid = uuid
+        self.customCocktails = customcocktails
+        self.collectionType = collectionType
+    }
+    
+    convenience init?(from dictionary: [String : Any]) {
+        guard let email = dictionary[Keys.email] as? String,
+              let uuid = dictionary[Keys.uuid] as? String,
+              let customCocktailsArray = dictionary[Keys.customCocktails] as? [[String : Any]],
+              let collectionType = dictionary[Keys.collectionType] as? String else { return nil }
+        let customCocktails = customCocktailsArray.compactMap({CustomCocktail(from: $0)})
+        self.init(email: email, uuid: uuid, customcocktails: customCocktails, collectionType: collectionType)
+    }
+}
+
+extension User: Equatable {
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.uuid == rhs.uuid
     }
 }
